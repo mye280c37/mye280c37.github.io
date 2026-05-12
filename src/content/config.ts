3e4646ff994c9c7aeb6a5c7df1a5f73401bbab9b
mye280c37 <mye785c83@gmail.com>
2026-05-12 21:18:13 +0200

    feat: migrate cv to astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,22 +18,70 @@ const blogCollection = defineCollection({
   }),
 });
 
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
+const publications = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      authors: z.array(z.string()),
+      date: z.coerce.date(), // 'yyyy-mm' format
+      venue: z.string(),
+      doi: z.string().url().optional(),
+      pdfUrl: z.string().url().optional(),
+      tags: z.array(z.string()).optional(),
+    })
+  ),
 });
+
+const projects = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      summary: z.string(),
+      description: z.string().optional(),
+      links: z.object({
+        github: z.string().url().optional(),
+        website: z.string().url().optional(),
+        wiki: z.string().url().optional(),
+      }).optional(),
+      tags: z.array(z.string()).default([]),
+      status: z.string().optional().default("Ongoing"),
+    })
+  ),
+});
+
+const interests = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      items: z.array(z.string()),
+    })
+  ),
+});
+
+
+const news = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      date: z.coerce.date(), // 'yyyy-mm-dd' format
+      emoji: z.string(),
+      title: z.string(),
+      description: z.string().optional(),
+      link: z.string().url().optional(),
+    })
+  ),
+});
+
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
-  'team': teamCollection,
+  'publications': publications,
+  'projects': projects,
+  'interests': interests,
+  'news': news,
 };
